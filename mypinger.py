876e6acf1b7ff83de69ba6796eab30519ef6fac4
MYYener <54672262+MYYener@users.noreply.github.com>
2019-10-04 07:37:08 +0200

    Commented out prints

--- mypinger.py
+++ mypinger.py
@@ -1,4 +1,5 @@
 # Module to ping hostnames and IP addresses
+# Not PEP8 compliant
 import subprocess
 
 __author__  =   'MUYE'
@@ -6,32 +7,33 @@ __company__ =   ''
 __version__ =   '01.00'
 __script__  =   'MYPinger'
 
+
 def ping(host, platform):
     if platform in ['Windows', 'windows', 'win']:
-        print('INFO: ' + __script__ + ': Windows platform chosen.')
-        print('INFO: ' + __script__ + ': Pinging ' + host + '.')
+        # print('INFO: ' + __script__ + ': Windows platform chosen.')
+        # print('INFO: ' + __script__ + ': Pinging ' + host + '.')
         process = subprocess.Popen(["ping", "-n", "3", host], stdout=subprocess.PIPE)
         process_output = process.communicate()[0]
         if process.returncode == 0:
-            print('INFO: ' + __script__ + ': Pinging successful.')
+            # print('INFO: ' + __script__ + ': Pinging successful.')
             status = 'OK'
             ping.returncode = 1
         else:
-            print('INFO: ' + __script__ + ': Pinging not successful.')
+            # rint('INFO: ' + __script__ + ': Pinging not successful.')
             status = 'NOK'
             ping.returncode = 0
 
     elif platform in ['Linux', 'linux', 'lin', 'CentOS', 'centos']:
-        print('INFO: ' + __script__ + ': Linux/CentOS platform chosen.')
-        print('INFO: ' + __script__ + ': Pinging ' + host + '.')
+        # print('INFO: ' + __script__ + ': Linux/CentOS platform chosen.')
+        # print('INFO: ' + __script__ + ': Pinging ' + host + '.')
         process = subprocess.Popen(["ping", "-q", "-c", "3", host], stdout=subprocess.PIPE)
         process_output = process.communicate()[0]
         if process.returncode == 0:
-            print('INFO: ' + __script__ + ': Pinging successful.')
+            # print('INFO: ' + __script__ + ': Pinging successful.')
             status = 'OK'
             ping.returncode = 1
         else:
-            print('INFO: ' + __script__ + ': Pinging not successful.')
+            # print('INFO: ' + __script__ + ': Pinging not successful.')
             status = 'NOK'
             ping.returncode = 0
 
